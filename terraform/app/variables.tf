ccc1d6cad80df096c9ab5d9561d01cfa3070a4dd
Ivanishyn <roman.ivanishyn@vitechteam.com>
2024-10-22 12:49:03 +0300

    Check terraform

--- terraform/app/variables.tf
+++ terraform/app/variables.tf
@@ -2,3 +2,13 @@ variable "environment" {
     description = "The environment in which the infrastructure is being deployed"
     type        = string
 }
+
+variable "access_key" {
+  description = "access key"
+  type = string
+}
+
+variable "secret_key" {
+  description = "secret key"
+  type = string
+}
