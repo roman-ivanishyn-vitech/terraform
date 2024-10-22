1db202a1a32286551e21eff1ec0e340c2cad1d50
Ivanishyn <roman.ivanishyn@vitechteam.com>
2024-10-22 12:42:01 +0300

    Check terraform

--- terraform/backend/variables.tf
+++ terraform/backend/variables.tf
@@ -2,3 +2,16 @@ variable "environment" {
     description = "The environment in which the infrastructure is being deployed"
     type        = string
 }
+
+variable "access_key" {
+  description = "access key"
+  type = string
+}
+
+variable "secret_key" {
+  description = "secret key"
+  type = string
+}
+
+// Key : AKIAQOZL5THGZ6GXDR6U
+// Secret: 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
